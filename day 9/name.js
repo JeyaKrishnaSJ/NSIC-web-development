7deff538fa82d966d2e314508c8e4f262fd36f86
JeyaKrishnaSJ <jeyakrishnasj@gmail.com>
2024-07-08 21:19:09 +0530

    Completed

---
+++ day 9/name.js
@@ -0,0 +1,9 @@
+input = "jeya krishna";
+  rev = "";
+    
+    for(i=input.length-1;i>=0;i--)
+    {
+        rev+=input[i];
+    }
+    
+    console.log(rev);
---
+++ day 9/sum.js
@@ -0,0 +1,7 @@
+num=[1,2,3,4,5];
+sum=0;
+for(i=0;i<num.length;i++)
+{
+    sum+=num[i];
+}
+console.log(sum);
